b101b25dc8daee5d5eeb89280a6c17da2b9247aa
Joonsang <59277857+vanbasten97@users.noreply.github.com>
2020-03-12 20:22:02 +0900

    Create finding-kimseobang-in-seoul.py

---
+++ Level_1/finding-kimseobang-in-seoul.py
@@ -0,0 +1,8 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12919
+# Difficulty : Level 1
+
+def solution(seoul):
+  return "김서방은 %d에 있다"%(seoul.index("Kim"))
+
+# Example)
+# solution(["Jane","Kim"]) == "김서방은 1에 있다"
